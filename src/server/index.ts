5a36794086ad8e13a91a15f802ecc8add313196d
Nick Samson <me@nicksamson.com>
2021-05-21 02:17:14 -0700

    Added typeorm code

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,21 +1,29 @@
 import dotenv from "dotenv";
 
+import "reflect-metadata";
+
+import morgan from "morgan";
+
 const result = dotenv.config();
 if (result.error) {
     throw result.error;
 }
 
 import express from "express";
-import {StatusCodes} from "http-status-codes";
+import {apiRouter} from "./api.js";
+import {loggerMiddleware, baseLogger} from "./log.js";
 
 const app = express();
 const port = Number.parseInt(process.env.SERVER_PORT || "3000");
+const logger = morgan("dev");
 
-app.post('/api/login', (req, res) => {
-    res.status(StatusCodes.UNAUTHORIZED)
-        .send('Hello World!');
-});
+app.use(logger);
+app.use(loggerMiddleware);
+app.use(express.json());
+app.use("/api", apiRouter);
 
 app.listen(port, "localhost", () => {
-    console.log(`Listening on port ${port}`)
+    baseLogger.info(`Listening on port ${port}`)
 });
+
+export default {};
